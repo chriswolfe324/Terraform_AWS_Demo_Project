650f783b32ff36f7f30e0229e6abf29fa71fecfe
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-25 09:15:31 -0500

    Initialized TF. Added variables.tf

--- providers.tf
+++ providers.tf
@@ -1,18 +1,18 @@
 terraform {
-    required_version = ">=1.14.4"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>6.33.0"    # correct version constraint ? 
-        }
+  required_version = ">=1.14.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.33.0" # correct version constraint ? 
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    default_tags {
-        tags = {
-            Project = "Terraform and AWS Demo"
-        }
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Project = "Terraform and AWS Demo"
     }
+  }
 }
