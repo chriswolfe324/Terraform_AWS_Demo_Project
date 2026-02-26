5d734ce88026c7764e896566d0b91d1c134cb0b5
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-26 09:54:50 -0500

    Added internet gateway

--- main.tf
+++ main.tf
@@ -4,43 +4,51 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
 
   tags = {
-    Name = "Terraform and AWS Demo VPC"
+    Name    = "Terraform and AWS Demo VPC"
     Project = "Terraform and AWS Demo"
   }
 }
 
 resource "aws_subnet" "public1" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "Public Subnet 1"
+    Name    = "Public Subnet 1"
     Project = "Terraform and AWS Demo"
   }
 }
 
 resource "aws_subnet" "public2" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   tags = {
-    Name = "Public Subnet 2"
+    Name    = "Public Subnet 2"
     Project = "Terraform and AWS Demo"
   }
 }
 
 resource "aws_subnet" "private1" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   tags = {
-    Name = "Private Subnet 1"
+    Name    = "Private Subnet 1"
     Project = "Terraform and AWS Demo"
   }
 }
 
 resource "aws_subnet" "private2" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.4.0/24"
   tags = {
-    Name = "Private Subnet 2"
+    Name    = "Private Subnet 2"
+    Project = "Terraform and AWS Demo"
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name    = "Demo Project Internet Gateway"
     Project = "Terraform and AWS Demo"
   }
 }
