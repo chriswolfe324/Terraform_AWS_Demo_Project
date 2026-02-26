40cd1d417df04251b4acee0207fbca360c030dea
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-26 09:47:49 -0500

    2 public/2 private subnets created

--- main.tf
+++ main.tf
@@ -8,3 +8,39 @@ resource "aws_vpc" "main" {
     Project = "Terraform and AWS Demo"
   }
 }
+
+resource "aws_subnet" "public1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Public Subnet 1"
+    Project = "Terraform and AWS Demo"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = "Public Subnet 2"
+    Project = "Terraform and AWS Demo"
+  }
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    Name = "Private Subnet 1"
+    Project = "Terraform and AWS Demo"
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  tags = {
+    Name = "Private Subnet 2"
+    Project = "Terraform and AWS Demo"
+  }
+}
