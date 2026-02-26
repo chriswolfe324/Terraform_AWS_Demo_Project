eee326985459527444852d91cedaa8489c2ae509
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-26 10:40:50 -0500

    Added route table associations for public1 and public2

--- main.tf
+++ main.tf
@@ -67,3 +67,13 @@ resource "aws_route" "route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gateway.id
 }
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.main.id
+}
