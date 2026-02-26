b48e6df37b25f7c7f4f0b0b446e5235c26ad9a0c
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-02-26 10:26:46 -0500

    Added route table and route to internet

--- main.tf
+++ main.tf
@@ -52,3 +52,18 @@ resource "aws_internet_gateway" "gateway" {
     Project = "Terraform and AWS Demo"
   }
 }
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "Demo Project Route Table"
+    Project = "Terraform and AWS Demo"
+  }
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
