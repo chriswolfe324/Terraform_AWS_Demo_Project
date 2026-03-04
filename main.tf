5b5150fc005dba2e0509c0c690f5f7f28748fbbc
Chris Wolfe <christopher.wolfe.324@gmail.com>
2026-03-04 09:21:47 -0500

    Created the load balancer listener

--- main.tf
+++ main.tf
@@ -566,3 +566,19 @@ resource "aws_lb" "Project_Load_Balancer" {
     Project = var.project_tag_name
   }
 }
+
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
+#Load Balancer Listener
+
+resource "aws_lb_listener" "Project_Listener" {
+  load_balancer_arn = aws_lb.Project_Load_Balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.Project_Target_Group.arn
+  }
+}
+
